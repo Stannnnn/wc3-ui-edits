861238dead309565a3b109984bb5fa8bba47c497
Stannnnn <stann@live.nl>
2023-01-29 21:35:37 +0100

    Changes

--- src/live.js
+++ src/live.js
@@ -1,93 +1,102 @@
-// Override defaults so we can add our hooks
-let sockets = []
-{
-    sockets = []
-    const nativeWebSocket = window.WebSocket
-    window.WebSocket = function (...args) {
-        const socket = new nativeWebSocket(...args)
-        sockets.push(socket)
-        return socket
-    }
-}
+const initMain = () => {
+    const sockets = []
 
-// Setup websocket hooks
-{
-    let a = setInterval(() => {
-        try {
+    // Override defaults so we can add our hooks
+    {
+        const nativeWebSocket = window.WebSocket
+        window.WebSocket = function (...args) {
+            const socket = new nativeWebSocket(...args)
+            sockets.push(socket)
+            return socket
+        }
+    }
+
+    // Setup websocket hooks
+    {
+        const a = setInterval(() => {
             if (sockets.length > 0) {
                 clearInterval(a)
                 initWebsocketHooks()
             }
-        } catch (e) {
-            console.log('Error caught:', e)
-        }
-    }, 1000)
+        }, 1000)
 
-    let friendGameActivityState = {}
+        const friendGameActivityState = {}
 
-    const initWebsocketHooks = () => {
-        const orig = sockets[0].onmessage
-        sockets[0].onmessage = (...args) => {
-            orig(...args)
+        const initWebsocketHooks = () => {
+            try {
+                const orig = sockets[0].onmessage
+                sockets[0].onmessage = (...args) => {
+                    orig(...args)
 
-            let e = JSON.parse(args?.[0]?.data)
+                    try {
+                        const e = JSON.parse(args?.[0]?.data)
 
-            switch (e?.messageType) {
-                case 'FriendsFriendUpdated': {
-                    const currentGame = e.payload.data.friend.localRichPresenceAttributes?.find(
-                        a => a.key === 'currentGameName'
-                    )
+                        switch (e?.messageType) {
+                            case 'FriendsFriendUpdated': {
+                                const currentGame = e.payload.data.friend.localRichPresenceAttributes?.find(
+                                    a => a.key === 'currentGameName'
+                                )
 
-                    if (
-                        currentGame?.value &&
-                        currentGame.value !== friendGameActivityState[e.payload.data.friend.battleTag]
-                    ) {
-                        friendGameActivityState[e.payload.data.friend.battleTag] = currentGame?.value
+                                if (
+                                    currentGame?.value &&
+                                    currentGame.value !== friendGameActivityState[e.payload.data.friend.battleTag]
+                                ) {
+                                    friendGameActivityState[e.payload.data.friend.battleTag] = currentGame?.value
 
-                        sockets[0].onmessage({
-                            data: JSON.stringify({
-                                messageType: 'ChatMessage',
-                                payload: {
-                                    message: {
-                                        content: `Your friend ${e.payload.data.friend.battleTag} entered a Warcraft III The Frozen Throne game called ${currentGame.value}`,
-                                        type: 'message',
-                                        sender: '',
-                                    },
-                                },
-                            }),
-                        })
+                                    sockets[0].onmessage({
+                                        data: JSON.stringify({
+                                            messageType: 'ChatMessage',
+                                            payload: {
+                                                message: {
+                                                    content: `Your friend ${e.payload.data.friend.battleTag} entered a Warcraft III The Frozen Throne game called ${currentGame.value}`,
+                                                    type: 'message',
+                                                    sender: '',
+                                                },
+                                            },
+                                        }),
+                                    })
+                                }
+
+                                friendGameActivityState[e.payload.data.friend.battleTag] = currentGame?.value
+                                break
+                            }
+                        }
+                    } catch (e) {
+                        console.log('Error caught:', e)
                     }
+                }
+            } catch (e) {
+                console.log('Error caught:', e)
+            }
+        }
+    }
 
-                    friendGameActivityState[e.payload.data.friend.battleTag] = currentGame?.value
+    const countInputs = () => {
+        return Array.from(document.getElementsByTagName('input')).filter(
+            e => !e.classList.contains('chat_focus_ignore')
+        )
+    }
 
-                    break
+    setInterval(() => {
+        // Forces the chat input to disable zoom (Zoom causes issues with auto scrolling down on new message). Also need to change the portals that apply to the ChatFrame
+        for (const v of [
+            ...document.getElementsByClassName('ChatFrame-Root'),
+            ...document.getElementsByClassName('PlayerList-PopupMenu'),
+        ]) {
+            v.style.zoom = 'unset'
+        }
+
+        // Forces the chat input to stay focused
+        const a = document.getElementById('chatPanelInput')
+
+        if (a) {
+            a.onblur = () => {
+                if (countInputs().length === 1) {
+                    document.getElementById('chatPanelInput')?.focus()
                 }
             }
         }
-    }
+    }, 1)
 }
 
-const countInputs = () => {
-    return Array.from(document.getElementsByTagName('input')).filter(e => !e.classList.contains('chat_focus_ignore'))
-}
-
-setInterval(() => {
-    // Forces the chat input to disable zoom (Zoom causes issues with auto scrolling down on new message). Also need to change the portals that apply to the ChatFrame
-    for (let v of [
-        ...document.getElementsByClassName('ChatFrame-Root'),
-        ...document.getElementsByClassName('PlayerList-PopupMenu'),
-    ]) {
-        v.style.zoom = 'unset'
-    }
-
-    // Forces the chat input to stay focused
-    let a = document.getElementById('chatPanelInput')
-
-    if (a) {
-        a.onblur = () => {
-            if (countInputs().length === 1) {
-                document.getElementById('chatPanelInput')?.focus()
-            }
-        }
-    }
-}, 1)
+initMain()
